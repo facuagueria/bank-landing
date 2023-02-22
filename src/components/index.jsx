76cb964b80e97cffc43122946f3d888158e73312
facuagueria <facundoagueria1@gmail.com>
2023-02-21 22:24:12 -0300

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,29 @@
+import Clients from "./Clients";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA.jsx";
+import Billing from "./Billing.jsx";
+import Business from "./Business";
+import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
+import Testimonials from "./Testimonials.jsx";
+import Footer from "./Footer.jsx";
+import GetStarted from "./GetStarted.jsx";
+import Stats from "./Stats.jsx";
+import FeedbackCard from "./FeedbackCard.jsx";
+import Button from "./Button.jsx";
+
+export {
+  Clients,
+  CardDeal,
+  CTA,
+  Billing,
+  Business,
+  Navbar,
+  Hero,
+  Testimonials,
+  Footer,
+  GetStarted,
+  Stats,
+  FeedbackCard,
+  Button,
+};
